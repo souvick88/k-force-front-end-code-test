0111e2bd9fb591a76e11f52e179c3e1a64586854
souvick C <souvickdesign1@gmail.com>
2020-09-23 18:33:22 -0500

    generate enrollee component

---
+++ src/app/enrollee/enrollee.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EnrolleeComponent } from './enrollee.component';
+
+describe('EnrolleeComponent', () => {
+  let component: EnrolleeComponent;
+  let fixture: ComponentFixture<EnrolleeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EnrolleeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EnrolleeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
